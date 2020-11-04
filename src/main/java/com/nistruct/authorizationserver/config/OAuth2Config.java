0dad68c2d1fa8255ca36cbb1a3642514caf9f301
marezi <markoilic22122012@gmail.com>
2020-11-04 10:30:34 +0100

    Token database finished

--- src/main/java/com/nistruct/authorizationserver/config/OAuth2Config.java
+++ src/main/java/com/nistruct/authorizationserver/config/OAuth2Config.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,11 +12,8 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.OAuth2RequestFactory;
-import org.springframework.security.oauth2.provider.endpoint.TokenEndpointAuthenticationFilter;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
@@ -26,26 +24,24 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
     @Value("${config.oauth2.clientSecret}")
     private String clientSecret;
 
-    @Value("${config.oauth2.privateKey}")
-    private String privateKey;
-
-    @Value("${config.oauth2.publicKey}")
-    private String publicKey;
-
     private final PasswordEncoder passwordEncoder;
 
     @Qualifier("authenticationManagerBean")
     private final AuthenticationManager authenticationManager;
 
-    private final ClientDetailsService clientDetailsService;
-
     private final UserDetailsService userDetailsService;
 
-    public OAuth2Config(PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, ClientDetailsService clientDetailsService, UserDetailsService userDetailsService) {
+
+    private final TokenStore tokenStore;
+
+    public OAuth2Config(PasswordEncoder passwordEncoder,
+                        AuthenticationManager authenticationManager,
+                        UserDetailsService userDetailsService,
+                        TokenStore tokenStore) {
         this.passwordEncoder = passwordEncoder;
         this.authenticationManager = authenticationManager;
-        this.clientDetailsService = clientDetailsService;
         this.userDetailsService = userDetailsService;
+        this.tokenStore = tokenStore;
     }
 
     @Override
@@ -58,39 +54,29 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
         clients.inMemory().withClient(clientId).secret(passwordEncoder.encode(clientSecret))
                 .scopes("read", "write")
                 .authorizedGrantTypes("password", "refresh_token")
-                .accessTokenValiditySeconds(3600)
-                .refreshTokenValiditySeconds(18000);
+                .accessTokenValiditySeconds(30 * 60)
+                .refreshTokenValiditySeconds(7 * 24 * 60 * 60);
     }
+
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.tokenStore(tokenStore()).tokenEnhancer(tokenEnhancer())
-                .authenticationManager(authenticationManager).userDetailsService(userDetailsService);
+        endpoints
+                .tokenStore(tokenStore)
+                .reuseRefreshTokens(false)
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
     }
 
     @Bean
-    public OAuth2RequestFactory requestFactory() {
-        CustomOAuth2RequestFactory requestFactory = new CustomOAuth2RequestFactory(clientDetailsService);
-        requestFactory.setCheckUserScopes(true);
-        return requestFactory;
-    }
-
-    @Bean
-    public JwtAccessTokenConverter tokenEnhancer() {
-        JwtAccessTokenConverter converter = new CustomTokenEnhancer();
-        converter.setSigningKey(privateKey);
-        converter.setVerifierKey(publicKey);
-        return converter;
-    }
-
-    @Bean
-    public JwtTokenStore tokenStore() {
-        return new JwtTokenStore(tokenEnhancer());
-    }
-
-    @Bean
-    public TokenEndpointAuthenticationFilter tokenEndpointAuthenticationFilter() {
-        return new TokenEndpointAuthenticationFilter(authenticationManager, requestFactory());
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setSupportRefreshToken(true);
+        tokenServices.setTokenStore(tokenStore);
+        tokenServices.setAccessTokenValiditySeconds(30 * 60);
+        tokenServices.setRefreshTokenValiditySeconds(7 * 24 * 60 * 60);
+        return tokenServices;
     }
 
 }
